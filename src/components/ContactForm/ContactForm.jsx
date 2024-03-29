a353e92807be049900011383ad43c5dbed30a7d4
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-29 13:46:08 +0100

    createContactForm

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,62 @@
+import { useId } from 'react';
+import css from './ContactForm.module.css';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+// import { useDispatch } from 'react-redux';
+// import { addContact } from '../../redux/contactsSlice';
+
+import { IoIosContact } from "react-icons/io";
+
+
+const ContactSchema = Yup.object().shape({
+    name: Yup.string().min(3, 'To short...').max(50,'To long!').required('Required field!'),
+    number: Yup.string().min(3, 'To short...').max(50, 'To long!').required('Required field!')
+});
+
+export default function ContactForm() {
+
+    const nameId = useId();
+    const numberId = useId();
+
+    //  const dispatch = useDispatch();
+    
+    return (
+        <Formik
+            initialValues={{ id: '', name: '', number: '' }}
+            onSubmit={(values, actions) => {
+                // const newContact = {
+                //     id: '',
+                //     name: values.name,
+                //     number: values.number,
+                // };
+                // dispatch(addContact(newContact));
+                actions.resetForm();
+            }}
+            validationSchema={ContactSchema}
+        >
+            <Form className={css.form}>
+                <div className={css.field}>
+                    <label className={css.label} htmlFor={nameId}>
+                        Name: 
+                    </label>
+                    <Field type="text" name="name" id={nameId} placeholder="John Wick"/>
+                    <ErrorMessage className={css.error} name="name" component="p" />
+                </div>
+
+                <div className={css.field}>
+                    <label className={css.label} htmlFor={numberId}>
+                        Number: 
+                    </label>
+                    <Field type="text" name="number" id={numberId} placeholder="443-89-12" />
+                    <ErrorMessage className={css.error} name="number" component="p" />
+                </div>
+
+                <button className={css.button} type="submit">
+                    Add contact <IoIosContact />
+                </button>
+            </Form>
+        </Formik>
+    );
+}
