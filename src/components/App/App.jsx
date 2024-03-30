fa09bb2c83021d76380813dcae44cbdded04a2c8
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-30 18:04:15 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,35 @@
-import ContactForm from "../ContactForm/ContactForm"
-import SearchBox from "../SearchBox/SearchBox"
+import ContactForm from '../ContactForm/ContactForm'
+import SearchBox from '../SearchBox/SearchBox'
+import ContactList from '../ContactList/ContactList'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../../redux/contactsOps';
+import { selectError, selectLoading } from '../../redux/contactsSlice';
+
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+
 import css from './App.module.css'
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch]);
+
   return (
-    <>
+    <div className={css.container}>
       <h1 className={css.title}>Contact App</h1>
       <SearchBox />
       <ContactForm />
-    </>
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      <ContactList />
+      <Toaster />
+    </div>
   )
 }
