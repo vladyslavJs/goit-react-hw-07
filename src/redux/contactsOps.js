e004deef2b56010aa4c841417e039e8e73d64f9a
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-30 18:05:28 +0100

    Create contactsOps.js

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://6606b15ebe53febb857e71ac.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get('/contacts');
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const addContact = createAsyncThunk('contacts/addContact', async (newContact, thunkAPI) => {
+  try {
+    const response = await axios.post('/contacts', newContact);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
